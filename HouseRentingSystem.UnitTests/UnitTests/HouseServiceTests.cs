3411d921585c5dd43a79a56e747786478f2b1716
Borislav Mihaylov <borvalorant@gmail.com>
2025-02-22 22:12:44 +0200

    update

--- HouseRentingSystem.UnitTests/UnitTests/HouseServiceTests.cs
+++ HouseRentingSystem.UnitTests/UnitTests/HouseServiceTests.cs
@@ -24,7 +24,7 @@ namespace HouseRentingSystem.Tests.UnitTests
             // Arrange: create a variable for the search term
             var searchTerm = "First";
 
-            // Act: invoke the service method with the term
+            // Act: invoke the service method with the termsadsadas
             var result = this.houseService.All(null, searchTerm);
 
             // Assert the returned houses' count is correct
